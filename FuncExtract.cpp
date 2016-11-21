7b88e47c1e5390c0d90cb4a7406f22fdf612834d
Alex <alex@mail.com>
2016-11-21 15:59:41 +0100

    using debug data to detect declarations

--- FuncExtract.cpp
+++ FuncExtract.cpp
@@ -1,15 +1,21 @@
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/Support/Dwarf.h"
 #include "llvm/Pass.h"
 #include "llvm/Analysis/RegionPass.h"
 #include "llvm/IR/Instructions.h"
+#include "llvm/IR/IntrinsicInst.h"
 #include "llvm/IR/Function.h"
 #include "llvm/Support/CommandLine.h"
 #include "llvm/ADT/DenseSet.h"
+#include "llvm/ADT/DenseMap.h"
 #include "llvm/ADT/StringMap.h"
+#include "llvm/IR/Metadata.h"
+#include "llvm/IR/DebugInfoMetadata.h"
 #include <fstream>
 #include <vector>
 #include <deque>
 #include <string>
+#include <limits>
 
 
 
@@ -20,6 +26,64 @@ static cl::opt<std::string> BBListFilename("bblist",
 	   		cl::value_desc("filename"), cl::Required  );
 
 namespace {
+
+	static bool  				  isEarlyReturnBlock(const BasicBlock *);
+	static DenseSet<BasicBlock *> findEarlyReturnsInRegion(const Region *);
+	static std::pair<unsigned,unsigned> getRegionLoc(DenseSet<BasicBlock *>&);
+	static void getVariableDebugInfo(Function *, DenseMap<Value *, DILocalVariable *>&);
+	static bool variableDeclaredInRegion(Value *, const std::pair<unsigned,unsigned>&, const DenseMap<Value *, DILocalVariable *>&);
+										 
+
+	static std::pair<unsigned,unsigned> getRegionLoc(const Region *R) {
+		unsigned min = std::numeric_limits<unsigned>::max();
+		unsigned max = std::numeric_limits<unsigned>::min();
+		
+		for (auto blockIter = R->block_begin(); blockIter != R->block_end(); ++blockIter) 
+		for (auto instrIter = (*blockIter)->begin(); instrIter != (*blockIter)->end(); ++instrIter) {
+			const DebugLoc& x = instrIter->getDebugLoc(); //FIXME invalid debugloc? 
+			if (x) {
+				min = std::min(min, x.getLine());
+				max = std::max(max, x.getLine());
+			}
+		}
+		errs() << min << " " << max << "\n";
+		return std::pair<unsigned,unsigned>(min, max);
+	}
+
+	// Detects basic blocks that only serve as return, meaning instructions in it
+	// only do memory reads, bitcasting and returning.
+	static bool isEarlyReturnBlock(const BasicBlock *BB) {
+		if (!isa<ReturnInst>(BB->getTerminator())) { return false; }
+		for (auto it = BB->begin(); it != BB->end(); ++it) {
+			if ( !isa<BitCastInst>(*it) && 
+				 !isa<ReturnInst>(*it)  && 
+				 !isa<LoadInst>(*it)    && 
+				 !isa<GetElementPtrInst>(*it) ) { return false; }
+		}
+		return true;
+	}
+
+
+	// finds basic blocks in the region that can terminate early. We need this 
+	// to determine where the function returns and modify C-file accordingly 
+	// (i.e. when returned, we set RET_EARLY flag so that we can bail afterwards )
+	static DenseSet<BasicBlock *> findEarlyReturnsInRegion(const Region *R) {
+		DenseSet<BasicBlock *> out;
+		Function *F = R->getEntry()->getParent();
+		for (auto a = F->begin(); a != F->end(); ++a) {
+			BasicBlock *retb = (&*a);
+			if (!isEarlyReturnBlock(retb)) { continue; }
+			for (auto b = R->block_begin(); b != R->block_end(); ++b) 
+			for (auto c = succ_begin(*b)  ; c != succ_end(*b)  ; ++c) {
+				if (retb == *c) {
+					out.insert(*b); 
+				}
+			}
+		}
+
+		return out;
+	}
+
 
 	//@return - list of basic block labels
 #if 0
@@ -155,6 +219,9 @@ namespace {
 
 
 
+	
+	typedef void (EnqueueBlockFunc)(std::deque<BasicBlock *>, BasicBlock *);
+
 	enum DFSDirection { SUCC, PRED };
 	static DenseSet<BasicBlock *> DFS(BasicBlock *BB, enum DFSDirection d) {
 		DenseSet<BasicBlock *> blocks; 
@@ -228,8 +295,7 @@ namespace {
 	}
 
 	static void analyzeFunctionArguments(Region *R, 
-										 DenseSet<Value *>& inputargs,
-										 DenseSet<Value *>& outputargs) {
+										 DenseSet<Value *>& inputargs) {
 		Function *F = R->getEntry()->getParent();
 		for (auto it = F->arg_begin(); it != F->arg_end(); ++it) {
 			inputargs.insert(&*it);
@@ -241,7 +307,9 @@ namespace {
 								const DenseSet<BasicBlock *>& predecessors,
 								const DenseSet<BasicBlock *>& successors,
 								DenseSet<Value *>& inputargs, 
-								DenseSet<Value *>& outputargs) {
+								DenseSet<Value *>& outputargs,
+								const std::pair<unsigned,unsigned>& regionBounds,
+								const DenseMap<Value *, DILocalVariable *>& debugInfo) {
 		DenseSet<Value *>sources = DFSInstruction(I);	
 		for (auto it = sources.begin(); it != sources.end(); ++it) {
 			if (auto instr = dyn_cast<AllocaInst>(*it)) {
@@ -250,31 +318,74 @@ namespace {
 				// is actually used (stored into, etc), if we did that would cause problems 
 				// for stack-allocated arrays as those can be uninitialized.
 				if (predecessors.find(instr->getParent()) != predecessors.end()) {
-					inputargs.insert(instr);
+					if (!variableDeclaredInRegion(instr, regionBounds, debugInfo)) 
+						inputargs.insert(instr);
 				} 
 				// if instruction is used by some instruction is successor basic block, we add 
 				// it to the output argument list only if I is store, i.e. we modify it. 
 				for (auto userit = instr->user_begin(); userit != instr->user_end(); ++userit) {
 					Instruction *userinstr = cast<Instruction>(*userit);	
 					BasicBlock *parentBB = userinstr->getParent(); 
+					if (isa<MemCpyInst>(I) && successors.find(parentBB) != successors.end()) {
+						if (variableDeclaredInRegion(instr, regionBounds, debugInfo)) 
+						outputargs.insert(instr);
+					}
 					if (isa<StoreInst>(I) && successors.find(parentBB) != successors.end()) {
-						if (mustReturnLocal(instr, cast<StoreInst>(I))) { 
-							outputargs.insert(instr); 
-						}
+						if (variableDeclaredInRegion(instr, regionBounds, debugInfo)) 
+						outputargs.insert(instr); 
 					}
 				}
 			}
+#if 0
 			if (auto global = dyn_cast<GlobalValue>(*it)) {
 				// globals are added to input argument list inconditionally. 
 				// if current instruction is store, we also add it to return values applying
 				// the same typing rules listed above
-				inputargs.insert(global);
-				if (isa<StoreInst>(I) && mustReturnGlobal(global, cast<StoreInst>(I))) {
+				inputargs.insert(global); // FIXME also pushes memcpy??
+				if (isa<StoreInst>(I)) {
 					outputargs.insert(global);
+				}
+			}
+#endif
+		}
+	}
+
+	// extracts debug metadata for every local variable and stores it in the map. We need this
+	// to determine where variables were originally declared.
+	static void getVariableDebugInfo(Function *F, DenseMap<Value *, DILocalVariable *>& map) {
+		for (BasicBlock& BB : F->getBasicBlockList()) 
+		for (Instruction& I :   (&BB)->getInstList()) {
+			if (!isa<AllocaInst>(&I)) 
+				continue; 
+
+			if (auto *LSM = LocalAsMetadata::getIfExists(&I)) 
+			if (auto *MDV = MetadataAsValue::getIfExists((&I)->getContext(), LSM)) {
+				for (User *U : MDV->users()) {
+					if (DbgDeclareInst *DDI = dyn_cast<DbgDeclareInst>(U)) {
+						DILocalVariable *local = DDI->getVariable();
+						std::pair<Value *, DILocalVariable *> kv(&I, local);
+						map.insert(kv);
+					}
 				}
 			}
 		}
 	}
+
+
+	static bool variableDeclaredInRegion(Value *V, const std::pair<unsigned,unsigned>& regionBounds, 
+										 const DenseMap<Value *, DILocalVariable *>& debugInfo) { 
+		auto iterator = debugInfo.find(V);
+		if (iterator == debugInfo.end()) {
+			errs() << "Could not locate variable info: \n";
+			V->dump();
+			return false;
+
+		}
+		DILocalVariable *DLV = iterator->getSecond();
+		unsigned line = DLV->getLine();
+		return (regionBounds.first <= line && line <= regionBounds.second);
+	}
+										 
 
 
 	struct FuncExtract : public RegionPass {
@@ -299,6 +410,8 @@ namespace {
 
 			errs() << "Found region!\n";
 
+			std::pair<unsigned,unsigned> regionBounds = getRegionLoc(R);
+
 			BasicBlock *b = R->getEntry();
 			DenseSet<BasicBlock *> predecessors = DFS(b, PRED); 
 			removeOwnBlocks(predecessors, R);
@@ -306,22 +419,124 @@ namespace {
 			DenseSet<BasicBlock *> successors = DFS(b, SUCC);
 			removeOwnBlocks(successors, R);
 
-
 			DenseSet<Value *> inputargs;
 			DenseSet<Value *> outputargs;
+			DenseMap<Value *, DILocalVariable *> debugInfo;
+
+			Function *F = R->getEntry()->getParent();
+			getVariableDebugInfo(F, debugInfo);
 
 			for (auto blockit = R->block_begin(); blockit != R->block_end(); ++blockit) 
 			for (auto instrit = blockit->begin(); instrit != blockit->end(); ++instrit) {
 				Instruction *I = &*instrit;
-				if (!isa<StoreInst>(I) && !isa<LoadInst>(I)) { continue; }
-				analyzeOperands(I, predecessors, successors, inputargs, outputargs);
+				if (!isa<StoreInst>(I) && !isa<LoadInst>(I) && !isa<MemCpyInst>(I)) { continue; }
+				analyzeOperands(I, predecessors, successors, inputargs, outputargs, regionBounds, debugInfo );
 			}
 
+#if 0
 			if (includesEntryBasicBlock(R)) {
-				analyzeFunctionArguments(R, inputargs, outputargs);
+				//analyzeFunctionArguments(R, inputargs);
+			}
+#endif
+			
+
+
+#if 0
+			for (auto it = inputargs.begin(); it != inputargs.end(); ++it) {
+				auto *L = LocalAsMetadata::get(*it);
+				auto *MDV = MetadataAsValue::get((*it)->getContext(), L);
+				for (User *U : MDV->users()) {
+					DbgDeclareInst *DDI = dyn_cast<DbgDeclareInst>(U);
+					// gives us <Value *, DbgDeclareInstr *> pair
+					if (DDI) {
+						DILocalVariable *localVar = DDI->getVariable();
+						(*it)->dump();
+						errs() << localVar->getName() << "\n";
+						errs() << localVar->getLine() << "\n";
+						Metadata *m = localVar->getType();
+						if (DIBasicType *DIBT = dyn_cast<DIBasicType>(m)) {
+							errs() << DIBT->getName() << "\n";
+						}
+						if (DICompositeType *DICT = dyn_cast<DICompositeType>(m)) {
+							if (DICT->getTag() == dwarf::DW_TAG_structure_type) {
+
+								errs() << "struct " << DICT->getName() << "\n";
+							}
+						}
+
+
+						if (DIDerivedType *DICT = dyn_cast<DIDerivedType>(m)) {
+							if (DICT->getTag() == dwarf::DW_TAG_pointer_type) {
+								errs() << "pointer " << DICT->getName() << "\n";
+
+								int ptrcount = 1;
+								//gotta reach the type that is not tagged as pointer...
+								Metadata *t = DICT->getRawBaseType();
+								while (true) {
+									if (auto *x = dyn_cast<DIDerivedType>(t)) {
+										t = x->getRawBaseType();
+										ptrcount++;
+										continue;
+									}
+
+									if (auto *x = dyn_cast<DICompositeType>(t)) {
+										errs() << x->getName() << " " << ptrcount << "\n";
+										break;
+									}
+
+									if (auto *x = dyn_cast<DIBasicType>(t)) {
+										errs() << x->getName() << " " << ptrcount << "\n";
+										break;
+									}
+								}
+
+
+
+
+								if (auto t2 = dyn_cast<DIDerivedType>(t)) {
+									t2->dump();
+								}
+
+
+
+
+								//(*it)->dump();
+							}
+						}
+
+					}
+				}
+
+
+				errs() << "--\n";
+			}
+#endif
+			
+
+
+#if 0
+			Function *F = R->getEntry()->getParent();
+			for (auto bit = F->begin(); bit != F->end(); ++bit) {
+				BasicBlock *bb = &*bit;
+				for (auto iit = bb->begin(); iit != bb->end(); ++iit) {
+					if (auto instr = dyn_cast<DbgDeclareInst>(&*iit)) {
+
+
+
+
+
+					}
+				}
+			}
+#endif
+
+#if 0
+				if (auto instr = dyn_cast<Instruction>(*it)) {
+					analyzeMetadata(instr);
+				}
 			}
 
-
+#endif
 
 			errs() << "in args: \n";
 			for (auto it = inputargs.begin(); it != inputargs.end(); ++it) {
