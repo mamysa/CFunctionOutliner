78a2208d6675721e18435e3c93d3ac08b4b9b519
mamysa <alexmamyshev@fastmail.com>
2017-01-16 22:35:00 +0100

    const-qualified test

---
+++ extractor/tests/const-qualifier-1/main.c
@@ -0,0 +1,15 @@
+int main() {
+	int a = 12;
+	int b = 14;
+	const int aconst = a; 
+
+	int out = 0;
+	if (aconst == 12) { out += 1; }
+	else { out += 2; }
+
+	const int bconst = b;	
+	if (bconst == 15) { out += 1; }
+	else { out += 2; }
+	
+	return out += bconst;
+}
